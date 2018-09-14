6afbd99214554976747eb6cc4df228129eb81126
ZainabOmar <zainab.hammami.zh@gmail.com>
2018-09-14 20:26:49 +0300

    adding main page

--- rbk-website/src/app/app.module.ts
+++ rbk-website/src/app/app.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { LayoutComponent } from './components/home/layout/layout.component';
+import { MainComponent } from './components/home/main/main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LayoutComponent
+    MainComponent
   ],
   imports: [
     BrowserModule
--- rbk-website/src/app/modules/routing/routing.module.ts
+++ rbk-website/src/app/modules/routing/routing.module.ts
@@ -1,5 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from '../../components/home/home.component';
+import { MainComponent } from '../../components/home/main/main.component';
+
+const appRouts: Routes = [
+  { path: '', component: MainComponent },
+
+];
 
 @NgModule({
   imports: [
